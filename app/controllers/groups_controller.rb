4f3ad336dabc628162847ba6c5d66632d6e9fb18
moroball14 <daiki.mr.0330@gmail.com>
2019-10-07 14:21:43 +0900

    display group-name to side-bar

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,20 +1,30 @@
 class GroupsController < ApplicationController
+  def index
+  end
   
   def new
+    @group = Group.new
+    @group.users << current_user
   end
   
   def create
-      # Group.create(groups_params)
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @group = Group.update(group_params)
   end
 
-  # private
-  # def group_params
-  #   params.require(group).permit(:name)
-  # end
+  private
+  def group_params
+    params.require(:group).permit(:name, { :user_ids => [] })
+  end
 end
