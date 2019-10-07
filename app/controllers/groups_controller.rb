c4c07f7438192cbffbcd90a88fcf87585e91d8a0
moroball14 <daiki.mr.0330@gmail.com>
2019-10-07 10:08:02 +0900

    add routing & action for controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,16 @@
 class GroupsController < ApplicationController
+  def create
+      # Group.create(groups_params)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  # private
+  # def group_params
+  #   params.require(group).permit(:name)
+  # end
 end
