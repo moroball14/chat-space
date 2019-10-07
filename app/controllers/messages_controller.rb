8c4d46fd834a70b90b35548f90d36ee56abf8314
moroball14 <daiki.mr.0330@gmail.com>
2019-10-07 19:37:57 +0900

    add routing

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,17 @@
 class MessagesController < ApplicationController
+  
   def index
   end
+
+  def create
+    @message = Message.new
+    Message.create(message_params)
+    render "index"
+  end
+
+  private
+  def message_params
+    params.require.(:message).permit(:content)
+  end
+
 end
