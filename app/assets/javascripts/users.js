4cc4847a92a6253d4a42f9de6a46d9b0ddf82105
moroball14 <daiki.mr.0330@gmail.com>
2019-10-14 17:38:37 +0900

    add incremental search

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,59 @@
+$(function(){
+
+  function addHTML(data) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${data.user_name}</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${data.id}" data-user-name="${data.user_name}">追加</div>
+                </div>`;
+    return html;
+  }
+
+  function addChatMember(id, name) {
+    var html = `<div class='chat-group-user'>
+                  <input name='group[user_ids][]' type='hidden' value='${id}'>
+                  <p class='chat-group-user__name'>${name}</p>
+                  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+                </div>`
+    return html;
+  }
+
+  $('#user-search-field').on("keyup",function(e){
+    e.preventDefault();
+    if ($(this).val() != '') var name = $(this).val();
+    
+    $.ajax({
+      url: '/users',
+      type: 'GET',
+      data: ('keyword=' + name),
+      dataType: "json",
+      processData: false,
+      contentType: false
+    })
+
+    .done(function(datas){
+      if (datas.length !== 0) {
+        $('.user-search-add').parent().remove();
+        datas.forEach(function(data){
+          $('#user-search-result').append(addHTML(data)); // 検索結果を表示している
+        });
+      } else {
+        alert('ユーザー検索に失敗しました');
+        $('.user-search-add').parent().remove();
+      }
+    });
+    // .fail(function(){
+    //   $('#user-search-result').append('<div id="error">ユーザーを取得できません</div>');
+    // });
+  });
+
+  $(document).on("click",".user-search-add",function(data){ //追加を押したら「追加」を押した部分の要素を削除したい
+    $(this).parent().remove();
+    var id = $(this).attr('data-user-id');
+    var name = $(this).attr('data-user-name');
+    $('.chat-group-form__field--right').eq(2).append(addChatMember(id, name));
+  });
+
+  $(document).on("click",".user-search-remove",function(data){
+    $(this).parent().remove();
+  });
+});
